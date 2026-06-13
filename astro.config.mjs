c6accdc793333f8905dedae6847edada63eeb5ad
cesarleyto <cezsar@gmail.com>
2026-06-13 08:10:55 -0600

    Agregado GitHub Actions para despliegue automático

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.itesla.edu.mx', // Pon tu dominio aquí
+});
